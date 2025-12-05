887bd919cfe7d225edb894c591acc69771ae17cb
George Achieng <george.achieng@student.moringaschool.com>
2025-12-05 08:14:18 +0300

    Function calculates the average temperature of the 30 days.

--- index.js
+++ index.js
@@ -20,3 +20,46 @@ module.exports = {
     // avg_temperature_in_fahrenheit,
     // avg_temperature_in_celsius
 };
+
+
+const day1TempCelsius = 0;
+const day2TempCelsius = 25;
+const day3TempCelsius = 21.1;
+const day4TempCelsius = 18;
+const day5TempCelsius = 26.7;
+const day6TempCelsius = 15;
+const day7TempCelsius = 22.2;
+const day8TempCelsius = 28;
+const day9TempCelsius = 20;
+const day10TempCelsius = 20;
+const day11TempCelsius = 23.9;
+const day12TempCelsius = 23;
+const day13TempCelsius =27.8;
+const day14TempCelsius = 30;
+const day15TempCelsius = 18.3;
+const day16TempCelsius = 22;
+const day17TempCelsius = 25;
+const day18TempCelsius = 26;
+const day19TempCelsius = 25.6;
+const day20TempCelsius = 24;
+const day21TempCelsius = 22.8;
+const day22TempCelsius = 21;
+const day23TempCelsius = 26.1;
+const day24TempCelsius = 27;
+const day25TempCelsius = 21.7;
+const day26TempCelsius = 19;
+const day27TempCelsius = 23.3;
+const day28TempCelsius = 17;
+const day29TempCelsius = 24.4;
+const day30TempCelsius = 29;
+
+let tot_temperature_in_celsius = day1TempCelsius + day2TempCelsius + day3TempCelsius + day4TempCelsius + day5TempCelsius + day6TempCelsius + day7TempCelsius + day8TempCelsius + day9TempCelsius + day10TempCelsius + day11TempCelsius + day12TempCelsius + day13TempCelsius + day14TempCelsius + day15TempCelsius + day16TempCelsius + day17TempCelsius + day18TempCelsius + day19TempCelsius + day20TempCelsius + day21TempCelsius + day22TempCelsius + day23TempCelsius + day24TempCelsius + day25TempCelsius + day26TempCelsius + day27TempCelsius + day28TempCelsius + day29TempCelsius + day30TempCelsius;
+
+let average_temperature_in_celsius = tot_temperature_in_celsius / 30;
+console.log(average_temperature_in_celsius
+    
+)
+
+
+
+
